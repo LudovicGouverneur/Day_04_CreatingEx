9c87f6010b9a5cc0f292d0de6c2fb6842788ed14
LudovicGouverneur <ludovic.gouverneur@gmail.com>
2018-08-09 11:11:16 +0200

    Update 05_incr_str_str.js

--- 05_incr_str_str.js
+++ 05_incr_str_str.js
@@ -1,10 +1,10 @@
 debugger;
 
-str_1 = "1";
-let str_1_incr;
-{ // let str_1_incr = str_1++;
-  let step_1 = Number(str_1);
-  str_1_incr = step_1;
-  let step_2 = step_1 + 1; 
-  str_1 = step_2;
+let str_e = "e";
+let incr_str_e;
+{ // let incr_str_e = ++str_e;
+  let step_1 = Number(str_e);
+  let step_2 = step_1 + 1;
+  incr_str_e = step_2;
+  str_e = step_2;
 };
